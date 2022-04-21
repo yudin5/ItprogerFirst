7f6c764a5ca9b388722acf02061295650855f84c
yudin5 <yudin5@yandex.ru>
2022-04-21 14:04:08 +0300

    add pass and confirmPass edit text, exit button, handlers

--- app/src/main/java/com/itproger/MainActivity.java
+++ app/src/main/java/com/itproger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.itproger;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,17 +24,66 @@ public class MainActivity extends AppCompatActivity {
 
         TextView mainText = findViewById(R.id.main_text);
         Button btnSecond = findViewById(R.id.btn_second);
-        btnSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnSecond.setOnClickListener(view -> {
 //                showInfo(mainText.getText().toString(), btnSecond);
-                showInfoAlert("Вы хотите закрыть приложение?");
+            showInfoAlert("Вы хотите закрыть приложение?");
+        });
+
+        // Поле с паролем
+        EditText pass = findViewById(R.id.pass);
+        pass.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String password = pass.getText().toString();
+                if (!hasFocus) {
+                    // Скрываем клавиатуру при клике вне поля
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                if (!password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), password, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        // Поле с подтверждением пароля
+        EditText confirmPassEditText = findViewById(R.id.pass_confirm);
+        confirmPassEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String confirmPass = confirmPassEditText.getText().toString();
+                if (!hasFocus) {
+                    // Скрываем клавиатуру при клике вне поля
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                if (!confirmPass.isEmpty()) {
+                    String initialPass = pass.getText().toString();
+                    Toast.makeText(getApplicationContext(),
+                            confirmPass.equals(initialPass) ? "Пароли совпадают" : "Пароли не совпадают",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
     public void btnClick(View v) {
         showInfo(((Button) v).getText().toString(), ((Button) v));
+    }
+
+    public void btnExitClick(View v) {
+        createExitAlert("Выход из приложения", "Вы хотите выйти из приложения?");
+    }
+
+    private void createExitAlert(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("Да", (dialog, which) -> finish())
+                .setNegativeButton("Нет", (dialog, which) -> dialog.cancel());
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
     private void showInfoAlert(String text) {
